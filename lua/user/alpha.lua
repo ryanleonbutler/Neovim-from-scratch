36b6d14439e0fce88b95c1a4444a941821822373
Ryan Butler <butryan@amazon.com>
2022-02-07 16:08:38 +0200

    Login screen change and some additional colorschemes

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -5,12 +5,13 @@ end
 
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+  [[                       _           ]],
+  [[                      (_)          ]],
+  [[ _ __   ___  _____   ___ _ __ ___  ]],
+  [[| '_ \ / _ \/ _ \ \ / / | '_ ` _ \ ]],
+  [[| | | |  __/ (_) \ V /| | | | | | |]],
+  [[|_| |_|\___|\___/ \_/ |_|_| |_| |_|]],
+  [[                                   ]],
 }
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
@@ -28,7 +29,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "ryanbutler.online"
 end
 
 dashboard.section.footer.val = footer()
